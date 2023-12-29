4fb50b9ba05f7f358cae50365eae3f8e06ed5384
Jason Gray <jason0gray@googlemail.com>
2023-12-29 17:41:53 +0000

    footer update

--- resources/views/frontend/includes/footer.blade.php
+++ resources/views/frontend/includes/footer.blade.php
@@ -2,12 +2,36 @@
     <div class="footer-menu container py-5">
         <div class="row">
             <div class="col col-12 col-md-4">
-                <p>Footer</p>
+                <img class="img-fluid" src="" alt="Footer Logo" />
+            </div>
+
+            <div class="col col-12 col-md-4">
+                <h5>Example Menu</h5>
+
+                <ul class="list-unstyled">
+                    <li><a href="#">Example Link</a></li>
+                    <li><a href="#">Example Link</a></li>
+                    <li><a href="#">Example Link</a></li>
+                    <li><a href="#">Example Link</a></li>
+                    <li><a href="#">Example Link</a></li>
+                </ul>
+            </div>
+
+            <div class="col col-12 col-md-4">
+                <h5>Example Menu</h5>
+
+                <ul class="list-unstyled">
+                    <li><a href="#">Example Link</a></li>
+                    <li><a href="#">Example Link</a></li>
+                    <li><a href="#">Example Link</a></li>
+                    <li><a href="#">Example Link</a></li>
+                    <li><a href="#">Example Link</a></li>
+                </ul>
             </div>
         </div>
     </div>
 
-    <div class="footer-copyright container-fluid py-3">
-        <p class="mb-0">Copyright section</p>
+    <div class="footer-copyright container py-3">
+        <p class="mb-0">© 2024 Copyright: sasstest.io</p>
     </div>
 </footer>
